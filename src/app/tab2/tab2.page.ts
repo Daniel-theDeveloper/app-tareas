a29d8679abb506289e3766d01fa608642e9d00b4
DanielAlfaroG <152915556+Daniel-theDeveloper@users.noreply.github.com>
2024-01-31 15:39:53 -0600

    correcciones del diseño

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Todo } from '../todo';
 import { DatabaseService } from '../services/database.service';
 
 @Component({
@@ -8,10 +7,29 @@ import { DatabaseService } from '../services/database.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
-  tasks = this.database.getTasks();
-  newTask = '';
+  public datetime: any;
 
   constructor(
     private database: DatabaseService
   ) { }
+
+  ngOnInit() {
+    const date = new Date();
+
+    // Set the value of the datetime to 2 days
+    // before the current day
+    let dayChange = -2;
+
+    // If the day we are going to set the value to
+    // is in the previous month then set the day 2 days
+    // later instead so it remains in the same month
+    if (date.getDate() + dayChange <= 0) {
+      dayChange = -dayChange;
+    }
+
+    // Set the value of the datetime to the day
+    // calculated above
+    date.setDate(date.getDate() + dayChange);
+    this.datetime = date.toISOString();
+  }
 }
