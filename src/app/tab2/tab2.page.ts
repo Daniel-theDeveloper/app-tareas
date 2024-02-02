3aa82537f0122962bbee84fb3560d365b9e5e21c
DanielAlfaroG <152915556+Daniel-theDeveloper@users.noreply.github.com>
2024-02-02 10:54:21 -0600

    Revert "Diseño del tablero"
    This reverts commit 68948eb37ebe17ac8da4f265369229c035a1882d.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,38 +9,28 @@ import { CalendarComponent, CalendarMode } from 'ionic2-calendar'
 })
 export class Tab2Page {
   public datetime: any;
-  eventSource = [];
-  viewTitle: string = "";
-
-  calendar = {
-    mode: "month" as CalendarMode,
-    currentDate: new Date()
-  }
-
-  selectedDate!: Date;
-  @ViewChild(CalendarComponent) myCal!: CalendarComponent;
-
-  developmmentMode: boolean = true //Cambiar luego a false
-  descriptionTest: string = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui. Eaque, dicta.";
 
   constructor(
     private database: DatabaseService
   ) { }
 
-  /* ¿COMO SERA EL PROCESO PARA CAMBIAR LAS TAREAS POR FECHA
-    SELECCIONADA?
-    
-    1- Cada vez que se precione una fecha en el calendario,
-    se ejecutara un metodo.
-    2- Ese metodo, recibira como parametro, la fecha seleccionada
-    3- El metodo ejecutara una consulta a la base de datos donde
-    seleccionara las tareas que coincidan con la fecha seleccionada
-    4- Se actualizara el tablero con los nuevos datos
-    */
+  ngOnInit() {
+    const date = new Date();
 
-  ngOnInit() {}
+    // Set the value of the datetime to 2 days
+    // before the current day
+    let dayChange = -2;
 
-  changeDay(day: any) {
-    console.log("Cambiando fecha a: ", day) //borrar esto
+    // If the day we are going to set the value to
+    // is in the previous month then set the day 2 days
+    // later instead so it remains in the same month
+    if (date.getDate() + dayChange <= 0) {
+      dayChange = -dayChange;
+    }
+
+    // Set the value of the datetime to the day
+    // calculated above
+    date.setDate(date.getDate() + dayChange);
+    this.datetime = date.toISOString();
   }
 }
