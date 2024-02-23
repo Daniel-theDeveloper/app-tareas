0dc6f014447013e924266158730be51f40efafe5
DanielAlfaroG <152915556+Daniel-theDeveloper@users.noreply.github.com>
2024-02-22 20:58:50 -0600

    Diseño de la configuracion

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  selectedLanguaje: string = "Español"
+  isAlertOpen: boolean = false;
+  public alertButtons = [
+    {
+      text: 'No',
+      role: 'cancel',
+      handler: () => {
+        console.log('Mejor no');
+      },
+    },
+    {
+      text: '¡Si!',
+      role: 'confirm',
+      handler: () => {
+        console.log('Alert confirmed');
+      },
+    },
+  ];
 
   constructor() {}
+
+  clickMe(option: number) {
+    console.log("Test");
+    if (option === 3) {
+      this.isAlertOpen = true;
+    }
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isAlertOpen = isOpen;
+  }
 
 }
